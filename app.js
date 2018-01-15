41484d8589697ed277f598d98e45ca5c3662ed4b
moh-ansari <ibrahimali344@gmail.com>
2018-01-14 20:28:06 -0600

    Updated app.js

--- app.js
+++ app.js
@@ -2,17 +2,15 @@ var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 
-var userdataRoutes = require('./routes/userdata');
 var userlikesRoutes = require('./routes/userlikes');
 
 var app = express();
 //mongoose.connect('localhost:27017/admin');
-mongoose.connect('mongodb://root:root@ds251277.mlab.com:51277/project');
+mongoose.connect(process.env.MONGODB_URI);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/userdata', userdataRoutes);
 app.use('/userlikes', userlikesRoutes);
 
 module.exports = app;
